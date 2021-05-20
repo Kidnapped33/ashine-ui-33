3ddbcb9b9ea74085662dfbe1b9c2dbc29491965f
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2021-05-20 22:57:29 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    hello
+    <hr>
+    <Ashine/>
+</template>
+
+<script>
+    import Ashine from './components/Ashine.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            Ashine
+        }
+    }
+</script>
---
+++ src/components/Ashine.vue
@@ -0,0 +1,13 @@
+<template>
+    Ashine 的组件
+</template>
+
+<script>
+    export default {
+        name: "Ashine"
+    }
+</script>
+
+<style scoped>
+
+</style>
